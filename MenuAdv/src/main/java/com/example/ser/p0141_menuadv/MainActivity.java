9a6ece431e7091dbde8f07fc05ba6ee39976802f
xxjukxx <shahov_2004@mail.ru>
2015-08-26 14:12:45 +0400

    1

---
+++ MenuAdv/src/main/java/com/example/ser/p0141_menuadv/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ser.p0141_menuadv;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Элементы экрана
+    TextView tv;
+    CheckBox chb;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // находим эдементы
+        tv = (TextView) findViewById(R.id.textView);
+        chb = (CheckBox) findViewById(R.id.chbExtMenu);
+
+    }
+
+    // создание меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        // добавляем пункты меню
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    // обработка нажатий
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        StringBuilder sb = new StringBuilder();
+
+        // Выведем в TextView информацию о нажатом пункте меню
+        sb.append("Item Menu");
+        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+        sb.append("\r\n title: " + item.getTitle());
+        tv.setText(sb.toString());
+
+        return super.onOptionsItemSelected(item);
+    }
+}
